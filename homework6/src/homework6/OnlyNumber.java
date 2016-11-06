285d839fe713fe427e8abc4e4b33073a204c492d
Young soo Kim <qhdl301@gmail.com>
2016-11-06 17:56:16 +0900

    1~100까지 중복된 숫자 출력 못하게 하는 문제 수정!!

--- homework6/src/homework6/OnlyNumber.java
+++ homework6/src/homework6/OnlyNumber.java
@@ -5,29 +5,27 @@ import java.util.*;
 public class OnlyNumber {
 
 	Scanner s = new Scanner(System.in);
-    int num=0;
+	int num;
 	int array[] = new int[10];
 
 	void input() {
 		System.out.println("1~100사이의 숫자를 입력하시오.");
-		for(int i=0; i<10; i++)
-		{
-			System.out.println((i+1)+"번째 숫자:");
-			array[i]=s.nextInt();
-			if(array[i]!=num&&array[i]<100||array[i]<0){
-				num=array[i];
+		for (int i = 0; i < 10; i++) {
+			System.out.println((i + 1) + "번째 숫자:");
+			array[i] = s.nextInt();
+			for (int j = 0; j < i; j++) {
+				if (array[i] == array[j] || array[i] > 100 || array[i] < 1) {
+					System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
+					i--;
+				}
 			}
-			else{
-				System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
-				i--;
-			}	
 		}
 		print();
 	}
 
 	void print() {
-		for(int i=9; i>=0; i--){
-			System.out.println((i+1)+"번째 숫자는"+array[i]+"입니다.");
+		for (int i = 9; i >= 0; i--) {
+			System.out.println((i + 1) + "번째 숫자는" + array[i] + "입니다.");
 		}
 
 	}
